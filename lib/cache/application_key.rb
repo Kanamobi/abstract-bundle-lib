5245e7b91f33b5fffa7db4df860dc5148e382de0
Douglas Rossignolli <douglas@kanamobi.com.br>
2018-02-16 12:08:12 -0200

    version 0.1.16.4 fix

--- lib/cache/application_key.rb
+++ lib/cache/application_key.rb
@@ -7,7 +7,7 @@ module Cache
     set_cache prefix: 'application_key'
 
     # attributes
-    attr_accessor *%w[id uuid key name application_id]
+    attr_accessor *%w[id uuid key name application_id created_at updated_at]
 
     def initialize(params = {})
       set_params(params)
